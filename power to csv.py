d2b98ad25fcc17b26670e21334fbcc28c958081d
seranovic <ssseraphim@proton.me>
2025-10-18 13:24:38 +0200

    updated power column to be given in HH:MM:SS format, fixed printing of time to be given in same format

--- power to csv.py
+++ power to csv.py
@@ -3,19 +3,19 @@ import pandas as pd
 import time
 import sys
 
-def savedata(timeout: int = 10, identifier: str = 'eng110data')->pd.DataFrame:
+def savedata(timeout: int = 10, identifier: str = '')->pd.DataFrame:
 
 
     watts = []
     seconds = []
-
     for i in range(timeout):
         values = get_eng110_data()
         watts.append(values[2])
-        seconds.append(time.localtime())
+        clock = time.localtime()
+        seconds.append(f'{clock[3]}:{clock[4]}:{clock[5]}}')
         time.sleep(1)
         print(f'Watts value is {values[2]}')
-        print(f'Time is now {time.localtime()}')
+        print(f'Time is now {clock[3]}:{clock[4]}:{clock[5]}')
 
     data = pd.DataFrame({
         'Watts': watts,
@@ -28,7 +28,5 @@ def savedata(timeout: int = 10, identifier: str = 'eng110data')->pd.DataFrame:
 
 if __name__=='__main__':
     times = int(sys.argv[1])
-    identifier = sys.argv[2]
+    identifier = str(sys.argv[2])
     data = savedata(timeout=times)
-
-    ## TO DO: MAKE THE TIME COLUMN BE REAL TIME FOR EASY APPENDING WITH NVIDIA-SMI CSV

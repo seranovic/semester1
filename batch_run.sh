a8d3118bbb3693831e50c7217186dfd2a337511d
Nuhddy <noahpc@live.dk>
2025-12-09 19:37:30 +0100

    fix bracket expansion

--- batch_run.sh
+++ batch_run.sh
@@ -2,13 +2,12 @@
 
 prefix="pow9pow21"
 batches=3
-backends=("gamdpy" "gamdpy -a" "lammps" "lammps -g") # one or more of:
-                                                     # gamdpy, gamdpy -a, lammps, lammps -g
+backends=("gamdpy" "gamdpy -a" "lammps" "lammps -g")
 
 for b in "${backends[@]}"; do
-    for i in {1..$batches}; do
+    for i in $(seq 1 $batches); do
         python3 main.py -p "$prefix-$i" $b
-        if ! [[ $b == "${backends[-1]}" && $i -eq 3 ]]; then
+        if ! [[ $b == "${backends[@]: -1}" && $i -eq $batches ]]; then
             sleep 30
         fi
     done
